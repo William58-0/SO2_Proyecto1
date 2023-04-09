755076bb957d1175bab327206d9c31618f79ecfd
William58-0 <2797652900101@ingenieria.usac.edu.gt>
2023-04-08 23:40:56 -0600

    forma de que funcione con 32 bits

--- codigo/pruebas/main.go
+++ codigo/pruebas/main.go
@@ -2,47 +2,84 @@ package main
 
 import (
 	"syscall"
- 
+	"strconv"
 	"fmt"
 )
 
 // FUENTE: https://github.com/pbnjay/memory/blob/7b4eea64cf58/memory_linux.go
-func sysTotalMemory() uint64 {
+func sysTotalMemory64() uint64 {
 	in := &syscall.Sysinfo_t{}
 	err := syscall.Sysinfo(in)
 	if err != nil {
 		return 0
 	}
-	// If this is a 32-bit system, then these fields are
-	// uint32 instead of uint64.
-	// So we always convert to uint64 to match signature.
+
 	return uint64(in.Totalram) * uint64(in.Unit)
 }
 
-func sysFreeMemory() uint64 {
+func sysFreeMemory64() uint64 {
 	in := &syscall.Sysinfo_t{}
 	err := syscall.Sysinfo(in)
 	if err != nil {
 		return 0
 	}
-	// If this is a 32-bit system, then these fields are
-	// uint32 instead of uint64.
-	// So we always convert to uint64 to match signature.
+
 	return uint64(in.Freeram) * uint64(in.Unit)
 }
 
-func main() {
-	libre := sysFreeMemory();
-	total := sysTotalMemory();
+// ----------------------------- Para 32 bits
+func sysTotalMemory32() uint32 {
+	in := &syscall.Sysinfo_t{}
+	err := syscall.Sysinfo(in)
+	if err != nil {
+		return 0
+	}
 
+	return uint32(in.Totalram) * uint32(in.Unit)
+}
+
+func sysFreeMemory32() uint32 {
+	in := &syscall.Sysinfo_t{}
+	err := syscall.Sysinfo(in)
+	if err != nil {
+		return 0
+	}
+
+	return uint32(in.Freeram) * uint32(in.Unit)
+}
+
+//------------------------------------------
+
+func main() {
 	factorGb := 1024 * 1024 * 1024;
 
-	fmt.Println(libre);
-	fmt.Println(total);
+	// FUENTE: https://programming-idioms.org/idiom/160/detect-if-32-bit-or-64-bit-architecture/1981/go
+	if (strconv.IntSize==64) {
+		libre := sysFreeMemory64();
+		total := sysTotalMemory64();
 
-	libre1 := fmt.Sprintf("%.2f", (float64(libre) / float64(factorGb)))
-	total1 := fmt.Sprintf("%.2f", (float64(total) / float64(factorGb)))
+		fmt.Println(libre);
+		fmt.Println(total);
 
-	fmt.Println(libre1);
-	fmt.Println(total1);
+		libre1 := fmt.Sprintf("%.2f", (float64(libre) / float64(factorGb)))
+		total1 := fmt.Sprintf("%.2f", (float64(total) / float64(factorGb)))
+
+		fmt.Println(libre1);
+		fmt.Println(total1);
+		
+	} else if(strconv.IntSize==32) {
+		libre := sysFreeMemory32();
+		total := sysTotalMemory32();
+
+		fmt.Println(libre);
+		fmt.Println(total);
+
+		libre1 := fmt.Sprintf("%.2f", (float64(libre) / float64(factorGb)))
+		total1 := fmt.Sprintf("%.2f", (float64(total) / float64(factorGb)))
+
+		fmt.Println(libre1);
+		fmt.Println(total1);
+	}
+
+	
 }
